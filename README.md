dbd1014f3e86f51ff7d39fb7c0fcb7c769bd5c24
ErikSlevin <107082107+ErikSlevin@users.noreply.github.com>
2023-01-07 21:32:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# auto-delete-folder
+
+Mithilfe dieses Skriptes lassen sich bei Windows Ordner bei einem Systemstart automatisch löschen. Ich benutze diesen Task, um täglich meinen Download-Ordner zu leeren.
+
+## Anleitung (Windows)
+
+1. Anpassen der Variabeln (`$ClearFolder`,`$logs` und`$csv`) im Skript [auto-delete-folder.ps1](auto-delete-folder.ps1)
+2. Windows > Aufgabenplanung > Bibliothek > Eigene > Neue Aufgabe erstellen
+3. Name und Beschreibung (individuell)
+4. Trigger: `Beim Starten des Computers`
+5. Aktion: `Programm starten`
+6. Programm/Skript: `powershell.exe` Argumente: `-ExecutionPolicy ByPass -NoProfile -NonInteractive -File  "PATH/TO/auto-delete-folder.ps1"`
+7. Eigenschaften öffnen: `Mit höchsten Privilegien ausführen`
+
+Somit wird bei jedem Systemstart der jeweilige Ordner gelöscht. Zusätzlich wird eine Historie (.csv und log-file) angelegt, welche Daten gelöscht wurden.
---
+++ auto-delete-folder.ps1
@@ -0,0 +1,47 @@
+﻿#Welcher Ordner soll automatisch geleert werden?
+$ClearFolder = "C:\Users\erikw\Downloads\"
+
+# Speicherort von der.log- und csv-File
+$logs = "C:\Users\erikw\skripte\logs\"
+$csv = "${$logs}download-autodelete.csv"
+
+# Aktuelles Datum und Uhrzeit
+$Datum = Get-Date -Format "dd.MM.yyyy"
+$Uhrzeit = "$(Get-Date -Format "HH:mm") Uhr"
+
+
+Start-Transcript -Path "${$logs}$(Get-Date -Format "yyyy-MM-dd-HH-mm")_download-autodelete.log"
+
+try{
+    # Wenn $ClearFolder existiert, lese alle Informationen ein
+    if (Test-Path -Path $ClearFolder){
+        $Inhalt = Get-FolderSize -BasePath $ClearFolder
+        $Inhalt = $Inhalt | Select-Object `
+            @{l="Datum"; e={$Datum}},`
+            @{l="Uhrzeit"; e={$Uhrzeit}},`
+            @{l="Datei"; e={$_.FolderName}},`
+            @{l="Dateigröße"; e={$_.SizeMB}}
+
+        # CSV-Übersicht erstellen, falls vorhanden die Informationen ergänzen
+        if (Test-Path $csv -PathType leaf){
+            $Inhalt | Export-csv -Append $csv -Encoding UTF8 -Delimiter ';'
+        } else {
+            $Inhalt | Export-csv $csv -Encoding UTF8 -Delimiter ';'
+        }
+        
+        # Alle Daten löschen
+        Remove-Item $ClearFolder* -Recurse -Force
+    } 
+}
+
+# Ausgabe Fehlermeldung
+catch{
+    Write-Host "$($Datum) | $($Uhrzeit) | $($_.Exception.Message)"
+}
+
+# Ausgabe für Log
+finally{
+    Write-Host "$($Datum) | $($Uhrzeit) | Download Autoclean erfolgreich ausgeführt!"
+}
+
+Stop-Transcript
