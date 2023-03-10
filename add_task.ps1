1e6eb5efddff6a6446bfcbab1b0c5e392940696f
ErikSlevin <107082107+ErikSlevin@users.noreply.github.com>
2023-03-10 22:34:37 +0100

    Update add_task.ps1

--- add_task.ps1
+++ add_task.ps1
@@ -7,7 +7,7 @@ $downloadPath = "$env:USERPROFILE\Skripte"
 $githubLink = "https://raw.githubusercontent.com/ErikSlevin/auto-delete-folder/main/auto-delete-folder.ps1"
 
 # Abfrage des Speicherorts fÃ¼r die heruntergeladene Datei
-$scriptPath = Read-Host "Geben Sie den Speicherort fÃ¼r die heruntergeladene Datei ein (oder lassen Sie das Standardverzeichnis $downloadPath)"
+$scriptPath = Read-Host "Geben Sie den Speicherort für die heruntergeladene Datei ein (oder lassen Sie das Standardverzeichnis $downloadPath)"
 
 # Verwendung des Standardverzeichnisses, wenn kein Pfad eingegeben wurde
 if ([string]::IsNullOrEmpty($scriptPath)) {
@@ -16,8 +16,7 @@ if ([string]::IsNullOrEmpty($scriptPath)) {
 
 Write-Host -ForegroundColor Gray "  | $scriptPath "
 
-
-# ÃœberprÃ¼fung, ob der angegebene Pfad vorhanden ist, andernfalls Erstellung der Ordnerstruktur
+# Überprüfung, ob der angegebene Pfad vorhanden ist, andernfalls Erstellung der Ordnerstruktur
 if (!(Test-Path -Path $scriptPath -PathType Container)) {
     Write-Host -ForegroundColor Gray "  | Erstelle Ordnerstruktur: $scriptPath"
     New-Item -Path $scriptPath -ItemType Directory -Force | Out-Null
@@ -25,12 +24,12 @@ if (!(Test-Path -Path $scriptPath -PathType Container)) {
 
 # Warnung an den Benutzer
 Write-Host ""
-Write-Warning "Bitte Ã¼berprÃ¼fen Sie die heruntergeladene Datei auf Schadsoftware, bevor Sie fortfahren!"
+Write-Warning "Bitte Überprüfen Sie die heruntergeladene Datei auf Schadsoftware, bevor Sie fortfahren!"
 
 # Herunterladen der Datei von Github
 Invoke-WebRequest -Uri $githubLink -OutFile "$scriptPath\auto-delete-folder.ps1"
 
-# ÃœberprÃ¼fen, ob die Datei erfolgreich heruntergeladen wurde
+# Überprüfen, ob die Datei erfolgreich heruntergeladen wurde
 if (Test-Path $scriptPath\auto-delete-folder.ps1) {
     Write-Host -ForegroundColor Gray "  | Die Datei 'auto-delete-folder.ps1' wurde erfolgreich unter '$scriptPath' gespeichert."
     
@@ -46,20 +45,25 @@ if (Test-Path $scriptPath\auto-delete-folder.ps1) {
     (Get-Content $scriptPath\auto-delete-folder.ps1) | ForEach-Object {
         $_ -replace '\$logs = .*', $newline
     } | Set-Content $scriptPath\auto-delete-folder.ps1
-    
-    $newlineDL = '$ClearFolder = ' + "'" + "$env:USERPROFILE\Downloads" + "\'"
-    (Get-Content $scriptPath\auto-delete-folder.ps1) | ForEach-Object {
-        $_ -replace '\$ClearFolder = .*', $newlineDL
-    } | Set-Content $scriptPath\auto-delete-folder.ps1
 
 } else {
     Write-Host -ForegroundColor Red "  | Beim Speichern der Datei ist ein Fehler aufgetreten."
 }
 
 # BestÃ¤tigung von Benutzer einholen
-$confirm = Read-Host "  | Haben Sie die heruntergeladene Datei Ã¼berprÃ¼ft und bestÃ¤tigen Sie, dass Sie fortfahren mÃ¶chten? (y/n)"
+$confirm = Read-Host "  | Haben Sie die heruntergeladene Datei Überprüft und bestätigen Sie, dass Sie fortfahren möchten? (y/n)"
 
 if ($confirm -eq "y") {
+
+    # PrÃ¼fen, ob das PSFolderSize-Modul installiert ist und es gegebenenfalls installieren
+    $moduleName = "PSFolderSize"
+    if (-not (Get-Module -Name $moduleName -ListAvailable)) {
+        Write-Host -ForegroundColor Gray "  | Das PowerShell-Modul PSFolderSize muss installiert werden (für ausführliches logging benötigt) "
+        Install-Module -Name $moduleName -Scope CurrentUser -Force
+    } else {
+        Write-Host -ForegroundColor Gray "  | Das PowerShell-Modul PSFolderSize bereits installiert (für ausführliches logging benötigt)"
+    }
+
     # Aufgabenspeicherort
     $taskPath = "\Eigene"
 
@@ -74,7 +78,7 @@ if ($confirm -eq "y") {
     Write-Host -ForegroundColor Gray "  | $taskName"
 
     # Beschreibung
-    $taskDescription = "Mithilfe dieses Skriptes wird der Inhalt eines Ordners beim Anmelden automatisch gelÃ¶scht (z.B. Download-Ordner)."
+    $taskDescription = "Mithilfe dieses Skriptes wird der Inhalt eines Ordners beim Anmelden automatisch gelöscht (z.B. Download-Ordner)."
 
     # Trigger
     $trigger = New-ScheduledTaskTrigger -AtLogOn
@@ -85,7 +89,7 @@ if ($confirm -eq "y") {
     # Aufgabe erstellen
     Register-ScheduledTask -TaskPath $taskPath -TaskName $taskName -Description $taskDescription -Trigger $trigger -Action $action -RunLevel Highest  | Out-Null
 
-    # ÃœberprÃ¼fung, ob die Aufgabe erfolgreich erstellt wurde
+    # Überprüfung, ob die Aufgabe erfolgreich erstellt wurde
     $taskExists = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
     
     if ($taskExists) {
@@ -97,10 +101,10 @@ if ($confirm -eq "y") {
         Write-Host -ForegroundColor Gray "  | Die Variable `$logs in der Datei 'auto-delete-folder.ps1' wurde erfolgreich geÃ¤ndert und das Log-Verzeichnis wurde erstellt!"
     }
     else {
-        Write-Host -ForegroundColor Red "  | Die Aufgabe '$taskName' konnte nicht erstellt werden. Bitte Ã¼berprÃ¼fen Sie Ihre Berechtigungen und versuchen Sie es erneut."
+        Write-Host -ForegroundColor Red "  | Die Aufgabe '$taskName' konnte nicht erstellt werden. Bitte überprüfen Sie Ihre Berechtigungen und versuchen Sie es erneut."
     }
 
-    # Ã–ffnen der Aufgabenplanung
+    # Öffnen der Aufgabenplanung
     $scheduleTaskPath = "\Microsoft\Windows\TaskScheduler\Tasks\$taskName"
     $scheduleTaskPath = $scheduleTaskPath.Replace("\", "\\")
     Start-Process taskschd.msc -ArgumentList "/s", "/tn", $scheduleTaskPath
